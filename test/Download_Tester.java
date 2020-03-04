14a3d71e4619891ed9cb9581258e4730dbfae923
ayushtripathi05 <50868698+ayushtripathi05@users.noreply.github.com>
2020-03-04 21:15:27 +0530

    Branch Initiation
    Initiation with Geek8080 code

--- test/Download_Tester.java
+++ test/Download_Tester.java
@@ -1,7 +1,5 @@
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,34 +12,29 @@ import java.util.logging.Logger;
  */
 public class Download_Tester {
 
-    public static void main(String[] args) throws IOException {
-        Download down = new Download("https://funksyou.com/fileDownload/Songs/128/27251.mp3");
-        down.start();
+    //public static Progress progress;
 
-        Thread thread = new Thread(new Runnable(){
-            @Override
-            public void run(){
-                Download down = new Download("https://funksyou.com/fileDownload/Songs/128/27251.mp3");
-                try {
-                    down.start();
-                } catch (IOException ex) {
-                    Logger.getLogger(Download_Tester.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
-        Thread thread2 = new Thread(new Runnable(){
-            @Override
-            public void run(){
-                Download down = new Download("https://funksyou.com/fileDownload/Songs/128/27251.mp3");
-                try {
-                    down.start();
-                } catch (IOException ex) {
-                    Logger.getLogger(Download_Tester.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
-        thread.start();
-        thread2.start();
+    public static void main(String[] args) throws IOException {
+        /*Download down = new Download("https://funksyou.com/fileDownload/Songs/128/27251.mp3");
+        progress = new Progress();
+        progress.setVisible(true);
+        //progress.prg.setValue(0);
+        if(down.start()==1){
+            progress.setVisible(false);
+            System.exit(0);
+        }
+         */
+        //Download down = new Download("https://www.researchgate.net/profile/Dr_DK_Kaushik/publication/264005162_An_Introduction_to_Microprocessor_8085/links/53fb5d750cf2364ccc03d728/An-Introduction-to-Microprocessor-8085.pdf");
+        //down.start();
+        Runnable r1 = new Download("https://www.researchgate.net/profile/Dr_DK_Kaushik/publication/264005162_An_Introduction_to_Microprocessor_8085/links/53fb5d750cf2364ccc03d728/An-Introduction-to-Microprocessor-8085.pdf");
+        Thread th1 = new Thread(r1);
+        //Thread th2 = new Thread(new Download("http://www.lamission.edu/lifesciences/lecturenote/AliPhysio1/Heart.pdf"));
+        Progress prg1 = new Progress((Download) r1);
+        prg1.setVisible(true);
+        //Progress prg2 = new Progress();
+        th1.run();
     }
 
 }
+
+
